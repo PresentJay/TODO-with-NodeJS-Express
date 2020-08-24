773b6bfbb1705f3ea21f7e1fd9874c13c72c49d5
PresentJay <PresentJay@Etri.re.kr>
2020-08-24 10:25:29 +0900

    feat: add get todo list function

--- TODO/routes/todo.js
+++ TODO/routes/todo.js
@@ -1,11 +1,35 @@
-var express = require('express');
-var router = express.Router();
+/* 
+    GET TODO
+ */
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('index', {
-        title: 'Express'
+// this module handles fileSystem
+var fs = require('fs');
+
+// get TODO LIST
+exports.list = function (req, res) {
+    // check presence of TODO-LIST Json file
+    fs.exists('./todo_list.json', function (exists) {
+        if (exists) {
+            // if it exists, read json file(get data)
+            fs.readFile('./todo_list.json', {
+                'encoding': 'utf8' // encoding this json context
+            }, function (err, list) { // handle errors
+                res.json(list);
+            });
+        } else {
+            // if it not exists, create new json file
+            var list = {
+                'list': []
+            };
+
+            // create file function(json)
+            fs.writeFile(
+                './todo_list.json', // file name
+                JSON.stringify(list), // stringify is js to JSON method
+                function (err) { // handle errors
+                    res.json(list);
+                }
+            );
+        }
     });
-});
-
-module.exports = router;
+};
